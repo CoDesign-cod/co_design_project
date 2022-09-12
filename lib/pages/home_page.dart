0bf387f178d145e1bd2fb2ac1d7a0d2ad181a5ba
Youssef <youssefessam135@hotmail.com>
2022-09-12 10:44:31 +0200

    git commit -m"ADD all files to github"

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class homePage extends StatelessWidget {
+  const homePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Co-Design'),
+      ),
+    );
+  }
+}
