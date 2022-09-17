fa7c81a90fbba0d606bd692be1301f889248f5f0
c1 <c1@email.com>
2022-09-17 06:06:08 +0200

    ADD scaffold to wallet page

---
+++ lib/pages/bottom_nav_bar_pages/wallet_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class walletPage extends StatefulWidget {
+  const walletPage({Key? key}) : super(key: key);
+
+  @override
+  State<walletPage> createState() => _walletPageState();
+}
+
+class _walletPageState extends State<walletPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
