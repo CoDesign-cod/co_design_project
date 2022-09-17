b030c4b541a6804975f7df13c07f29893c38f589
c1 <c1@email.com>
2022-09-17 06:13:10 +0200

    Modify from profile_page to nav_bar_profile_page

---
+++ lib/pages/bottom_nav_bar_pages/nav_bar_profile_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class navBarProfilePage extends StatelessWidget {
+  const navBarProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
