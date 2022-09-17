ba2b2d53d53b0e4b984938f2f5af9c0500c2875f
c1 <c1@email.com>
2022-09-17 06:04:58 +0200

    ADD scaffold to profile page

---
+++ lib/pages/bottom_nav_bar_pages/profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class profilePage extends StatefulWidget {
+  const profilePage({Key? key}) : super(key: key);
+
+  @override
+  State<profilePage> createState() => _profilePageState();
+}
+
+class _profilePageState extends State<profilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
