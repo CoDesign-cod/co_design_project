ca943c750a89af45a7f72998ed20a7a48ca0fa84
c1 <c1@email.com>
2022-09-17 06:21:23 +0200

    Modify theme color to grey

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.grey,
       ),
       debugShowCheckedModeBanner: false,
       home: homePage(),
