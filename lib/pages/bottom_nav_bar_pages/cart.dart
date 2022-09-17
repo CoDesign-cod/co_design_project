03178dd07c3d77bb7ccc203047ea04834bca2b0c
c1 <c1@email.com>
2022-09-17 13:43:45 +0200

    ADD scaffold to cart.dart

---
+++ lib/pages/bottom_nav_bar_pages/cart.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Cart extends StatelessWidget {
+  const Cart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
