2591b17e77f8030f0fc1f7170099c53258d73ae6
c1 <c1@email.com>
2022-09-17 14:25:57 +0200

    Modify navBarProfilePage to Profile in profile.dart

--- lib/pages/bottom_nav_bar_pages/profile.dart
+++ lib/pages/bottom_nav_bar_pages/profile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class navBarProfilePage extends StatelessWidget {
-  const navBarProfilePage({Key? key}) : super(key: key);
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
