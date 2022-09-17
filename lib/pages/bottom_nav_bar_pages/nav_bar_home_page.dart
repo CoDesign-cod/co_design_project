dbf611610a287693bf16249c7437af623c5fd375
c1 <c1@email.com>
2022-09-17 06:13:51 +0200

    ADD Scaffold to nav_bar_home_page

---
+++ lib/pages/bottom_nav_bar_pages/nav_bar_home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class navBarHomePage extends StatelessWidget {
+  const navBarHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
