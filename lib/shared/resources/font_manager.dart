4e9b42081ab388d339c454e3bd9f5af79df793e8
ahmed4442025 <ahmed4442025@gmail.com>
2022-09-18 19:07:50 +0200

    add font_manager

---
+++ lib/shared/resources/font_manager.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class FontConstant{
+  static const fontFamily = "Source Sans Pro";
+}
+
+class FontWeightManager{
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight regular= FontWeight.w400;
+  static const FontWeight light = FontWeight.w300;
+}
+
+class FontSizeManager{
+  static const double s10 = 10;
+  static const double s12 = 12;
+  static const double s14 = 14;
+  static const double s16 = 16;
+  static const double s17 = 17;
+  static const double s18 = 18;
+  static const double s20 = 20;
+  static const double s22 = 22;
+}
