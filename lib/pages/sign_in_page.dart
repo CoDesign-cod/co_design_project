b7f420a552a32996685d116b5ca0671da18eb8eb
Youssef <name@email.com>
2022-09-19 02:10:14 +0200

    ADD logo for sign_in_page.dart

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -12,8 +12,42 @@ class _signInPageState extends State<signInPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children:[
-
+        children: [
+          MaterialButton(
+            onPressed: () {},
+            child: Row(
+              children: [
+                Image(
+                  image: AssetImage(
+                    'assets/images/facebook_logo_icon.png',
+                  ),
+                  width: 29,
+                  height: 29,
+                ),
+                SizedBox(width: 11,),
+                Text('Facebook',style:TextStyle(color: Colors.white,fontSize:18,),),
+              ],
+            ),
+            color: Color(0xf1877F2),
+          ),
+          SizedBox(height: 24,),
+          MaterialButton(
+            onPressed: () {},
+            child: Row(
+              children: [
+                Image(
+                  image: AssetImage(
+                    'assets/images/google_logo_icon.png',
+                  ),
+                  width: 29,
+                  height: 29,
+                ),
+                SizedBox(width: 14,),
+                Text('Google Account',style:TextStyle(fontSize:15,),),
+              ],
+            ),
+            color: Colors.white,
+          ),
         ],
       ),
     );
