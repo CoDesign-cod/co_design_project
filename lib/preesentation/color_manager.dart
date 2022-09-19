6ba33d91da5d42c5355143fd958ba7679bc3e0e5
Youssef <name@email.com>
2022-09-19 04:53:51 +0200

    ADD color_manager.dart to a presentation layer

---
+++ lib/preesentation/color_manager.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorManager
+{
+  static Color primary = const Color(0xffFFFFFF);
+  static Color purple = const Color (0xff951B81);
+  static Color lightBlue = const Color(0xff2DAAE1);
+  static Color darkMode = const Color(0xff303030);
+  static Color lightGrey = const Color(0xffA8A8A8);
+  static Color darkBlue = const Color(0xff1877F2);
+  static Color greyScale = const Color(0xffF1F1F1);
+  static Color orange = const Color(0xffFFA200);
+  static Color viola = const Color(0xffC58CBC);
+  static Color blizzardBlue = const Color(0xffAAD9EE);
+
+}
