4a8c125a986fe23d4811aeaf6ddf4a942870a4fd
c11 <c11@email.com>
2022-09-19 05:32:55 +0200

    Move font_manager.dart from resources to presentation

--- lib/preesentation/font_manager.dart
+++ lib/preesentation/font_manager.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class FontConstant{
-  static const fontFamily = "Source Sans Pro";
+  static const String fontFamily = "Montserrat";
 }
 
 class FontWeightManager{
