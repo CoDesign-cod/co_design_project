0a489120fd11c338c28103a93840cc758ab7ecee
Youssef <youssefessam5623@gmail.com>
2022-09-19 17:41:09 +0200

    Modify fontWeightManager lines order

--- lib/presentation/font_manager.dart
+++ lib/presentation/font_manager.dart
@@ -5,11 +5,11 @@ class FontConstant{
 }
 
 class FontWeightManager{
-  static const FontWeight bold = FontWeight.w700;
-  static const FontWeight semiBold = FontWeight.w600;
-  static const FontWeight medium = FontWeight.w500;
-  static const FontWeight regular= FontWeight.w400;
   static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular= FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
 }
 
 class FontSizeManager{
